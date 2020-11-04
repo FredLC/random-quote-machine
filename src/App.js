40bba04ace8d3470e5bd75b45af8f2c266f96c00
fredlc <fredlefevre@outlook.com>
2020-11-03 19:15:08 -0500

    Initial layout and styles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper" id="quote-box">
+      <p className="quote" id="text">
+        <i class="fas fa-quote-left"></i>
+        Lyft taiyaki meditation pug green juice, bushwick la croix poke kinfolk bicycle rights iPhone small batch pinterest listicle fixie.
+        <i class="fas fa-quote-right"></i>
+      </p>
+      <p id="author"><em>- Joe Marambi</em></p>
+      <div className="buttons-wrapper">
+        <a href="https://twitter.com/intent/tweet" target="_blank" id="tweet-quote">Tweet Quote <i class="fab fa-twitter"></i></a>
+        <button id="new-quote">New Quote</button>
+      </div>
     </div>
   );
 }
